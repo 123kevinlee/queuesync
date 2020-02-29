720f3376778b8bea7ca802fffd4c9241bb510049
reachedcoding <MakesSenseForNow4141>
2020-02-29 18:27:11 -0500

    push

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ app.get('/', function (req,res) {
     console.log('Got here');
     res.redirect(spotify.getOauth2URL());
 });
+
 app.get('/callback', async function (req,res) {
     let authorization_code = req.query.code;
     let json = await spotify.getTokens(authorization_code);
     let access_token = json['access_token'];
     let refresh_token = json['refresh_token'];
-
+    
 });
 
 app.listen(80);
