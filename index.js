a1eee47d7dff3c026c06f694741e53f4b99e5abc
reachedcoding <MakesSenseForNow4141>
2020-03-01 02:09:44 -0500

    helper methods

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const Spotify = require('./spotify');
 let express = require('express');
 let app = express();
 var jwt = require('jsonwebtoken');
-let spotify = new Spotify();
 const dotenv = require('dotenv');
 var cookieParser = require('cookie-parser')
 dotenv.config();
 const private_key = process.env.PRIVATE_KEY;
 const path = require('path');
+const Track = require('./track');
+
+let sessions = [];
+
 app.use(cookieParser());
 app.use(express.static('site/'));
 app.use(express.static('site/partials/'));
+
+
 app.get('/', function (req, res) {
     let room_code = req.query.code;
     if (!room_code)
@@ -25,18 +30,21 @@ app.get('/', function (req, res) {
 app.set('view engine', 'ejs');
 
 app.get('/login', async function (req, res) {
-    res.redirect(spotify.getOauth2URL());
+    res.redirect(new Spotify().getOauth2URL());
 });
 
 app.get('/callback', async function (req, res) {
+    let room_code = makeid(8);
+    let session = new Session(room_code);
     let authorization_code = req.query.code;
-    let json = await spotify.getTokens(authorization_code);
+    let json = await session.spotify.getTokens(authorization_code);
     let access_token = json['access_token'];
     let refresh_token = json['refresh_token'];
     var token = jwt.sign({ access_token: access_token, refresh_token: refresh_token }, private_key);
-    spotify.setAccessToken(access_token);
-    spotify.setRefreshToken(refresh_token);
+    session.spotify.setAccessToken(access_token);
+    session.spotify.setRefreshToken(refresh_token);
     res.cookie('jwt', token);
+    sessions.push(session);
     res.redirect('/dashboard');
 });
 
@@ -56,7 +64,6 @@ app.get('/search', async function (req,res) {
 });
 
 app.get('/dashboard', async function (req, res) {
-    let code = makeid(8);
     let tracks = [];
     res.render(path.join(__dirname, "/site/views/dashboard.ejs"), {
         code: code,
@@ -74,6 +81,30 @@ app.get('/dashboard', async function (req, res) {
     //     } else {
     //         res.redirect('login');
     //     }
+});
+
+app.get('/tracks-playing', async function (req,res) {
+    let room_code = req.query.room_code;
+    let session = findSession(room_code);
+    return session.tracks;
+});
+
+app.get('/get-songs', async function (req,res) {
+    let room_code = req.query.room_code;
+    let query = req.query.query;
+    let session = findSession(room_code);
+    let tracks = session.findSongs();
+    return tracks;
+});
+
+app.post('/add-song', async function (req,res) {
+    let room_code = req.query.room_code;
+    let session = findSession(room_code);
+    let title = req.query.title;
+    let authors = req.query.authors;
+    let uri = req.query.uri;
+    let track = new Track(title, authors, uri);
+    session.TRACKS.add(track);
 });
 
 function decryptJWT(token) {
@@ -100,3 +131,9 @@ function makeid(length) {
     return result;
 }
 
+function findSession(room_code) {
+    for (let session in sessions) {
+        if (session.room_code == room_code) return session;
+    }
+    return null;
+}
