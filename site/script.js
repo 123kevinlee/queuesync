2fbe7ad4c9950ab5f3511c1cde9879111b385367
Kevin Lee <123kevinlee@gmail.com>
2020-03-01 10:07:08 -0500

    get/pull

--- site/script.js
+++ site/script.js
@@ -2,19 +2,21 @@ $(document).ready(function () {
     $("#search").click(function (e) {
         var queryString = document.getElementById("songKeywords").value;
         var code = document.getElementById("code").innerText.substring(10);
-        code = "VlXDQKlS";
-        alert(queryString);
-        alert(code);
+        code = "o8Tzfdkd";
+        alert("https://queuesync.tech/get-songs?room-code=" + code + "&query=" + queryString);
         e.preventDefault();
         $.ajax({
             type: "GET",
-            url: "https://queuesync.tech/get-songs?room_code=" + code + "$query=" + queryString,
+            url: "https://queuesync.tech/get-songs?room_code=" + code + "&query=" + queryString,
+            // url: "https://cors-anywhere.herokuapp.com/https://f9099312.ngrok.io/get-songs?room_code=TeqFDYYd&query=selena",
             json: true,
             success: function (result) {
+                alert("good");
                 var tableHtml = "<tr class=\"table-header\"" + "><th></th><th>Track</th><th>Artist</th></tr>";
                 for (var i = 0; i < 5; i++) {
-                    tableHtml += "<tr><th>" + (i + 1) + "</th><th>" + result.tracks[i].name + "</th><th>" + result.tracks.items.artist + "</th><th class=\"add-button\"<a href=\"#\"><span class=\"glyphicon glyphicon-plus\"></span></a></th></tr>";
+                    tableHtml += "<tr class=\"table-header\"><th>" + (i + 1) + "</th><th>" + result[i].name + "</th><th>" + result[i].artists + "</th><th class=\"add-button\" onclick = \"addSong(code,result[i].uri)\"><a href=\"#\"><span class=\"glyphicon glyphicon-plus\"></span></a></th></tr>";
                 }
+                document.getElementById('search-list').innerHTML = tableHtml;
             },
             error: function (result) {
                 alert('Error');
@@ -22,18 +24,53 @@ $(document).ready(function () {
         });
     });
 
-    $("#add").click(function (e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "https://queuesync.tech/add-song",
-            json: true,
-            success: function (result) {
-                alert('ok');
+    function addSong(code, songID) {
+        $.post("https://queuesync.tech/add-songs", {
+                room_code: code,
+                uri: songID
             },
-            error: function (result) {
-                alert('error');
-            }
-        });
-    });
+            function (data, status) {
+                console.log("Sent!");
+            });
+    }
+
+    // $('#search-list').on('click', 'a', function () {
+    //     var table = $("search-list");
+    //     console.log(table);
+    //     var data = table.row($(this).parents('tr')).data();
+    //     console.log("good");
+    //     var code = document.getElementById("code").innerText.substring(10);
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "https://queuesync.tech/get-songs?room_code=" + code + "&query=" + data[1],
+    //         json: true,
+    //         success: function (result) {
+    //             var songID = results[i].uri;
+    //         },
+    //         error: function (result) {
+    //             alert('Error');
+    //         }
+    //     });
+    //     $.post("https://queuesync.tech/", {
+    //             room_code: code,
+    //             uri: songID
+    //         },
+    //         function (data, status) {
+    //             console.log("Sent!");
+    //         });
+    // });
+    // // $("#add").click(function (e) {
+    // //     e.preventDefault();
+    // //     $.ajax({
+    // //         type: "POST",
+    // //         url: "https://queuesync.tech/add-song?room-code=" + code "",
+    // //         json: true,
+    // //         success: function (result) {
+    // //             alert('ok');
+    // //         },
+    // //         error: function (result) {
+    // //             alert('error');
+    // //         }
+    // //     });
+    // });
 });
